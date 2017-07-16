add879802bb81c9928acb1e96fd090d9e09cfb6b
Yugarek <yugaface@gmail.com>
2017-07-16 20:15:49 +0200

    Primera Version con BUSCADOR

--- src/app/registroview/registroview.component.ts
+++ src/app/registroview/registroview.component.ts
@@ -16,11 +16,16 @@ export class RegistroviewComponent {
 
   show: boolean = true;
   llave:any;
+  try:string = "";
 
   constructor(public db: AngularFireDatabase) {
 
   this.items = db.list('/prueba');
 
+  }
+  //buscador
+  buscador(){
+    console.log(this.try);
   }
   //añadiendo registro
   addItem(addNom: string, addApe: string, addTel: string) {
@@ -47,10 +52,84 @@ export class RegistroviewComponent {
   this.llave = "";
   }
   //activar modo edición
-  activarEditor(key,item){
-    this.llave = key;
+  activarEditor(item){
+    this.llave = item.$key;
     this.newnom = item.nom;
     this.newape = item.ape;
     this.newtel = item.tel;
   }
+  // Cancelar updateItem
+  updateCancel(){
+    this.llave = "";
+  }
+  // add 20
+  addTwuenty(){
+    this.items.push({ nom: "Juan",
+                      ape: "Lopez",
+                      tel: "987654325"});
+    this.items.push({ nom: "Miguel",
+                      ape: "Ferrer",
+                      tel: "765432198"});
+    this.items.push({ nom: "Antonio",
+                      ape: "Solis",
+                      tel: "654321987"});
+    this.items.push({ nom: "Felipe",
+                      ape: "Lorenzo",
+                      tel: "123456789"});
+    this.items.push({ nom: "Yeni",
+                      ape: "Forner",
+                      tel: "543219876"});
+    this.items.push({ nom: "Vicente",
+                      ape: "Torres",
+                      tel: "987654325"});
+    this.items.push({ nom: "Manuel",
+                      ape: "Sancho",
+                      tel: "219876543"});
+    this.items.push({ nom: "Pablo",
+                      ape: "Serrano",
+                      tel: "456789123"});
+    this.items.push({ nom: "Quique",
+                      ape: "Ramirez",
+                      tel: "987654325"});
+    this.items.push({ nom: "Jose Luis",
+                      ape: "Estobal",
+                      tel: "219876543"});
+    this.items.push({ nom: "Carlos",
+                      ape: "Ruiz",
+                      tel: "456789123"});
+    this.items.push({ nom: "Federico",
+                      ape: "Frances",
+                      tel: "659874123"});
+    this.items.push({ nom: "Juan",
+                      ape: "Lorente",
+                      tel: "123897456"});
+    this.items.push({ nom: "Juan",
+                      ape: "Soria",
+                      tel: "124578968"});
+    this.items.push({ nom: "Esteban",
+                      ape: "Reina",
+                      tel: "456589712"});
+    this.items.push({ nom: "Mariano",
+                      ape: "Lason",
+                      tel: "789987456"});
+    this.items.push({ nom: "Juan",
+                      ape: "Perez",
+                      tel: "122145547"});
+    this.items.push({ nom: "Milena",
+                      ape: "Enric",
+                      tel: "896325741"});
+    this.items.push({ nom: "Antonio",
+                      ape: "Sabadell",
+                      tel: "258741369"});
+    this.items.push({ nom: "John",
+                      ape: "Calle",
+                      tel: "987456326"});
+    this.items.push({ nom: "Toni",
+                      ape: "Valencia",
+                      tel: "987654325"});
+    this.items.push({ nom: "Jaime",
+                      ape: "Gutierrez",
+                      tel: "654321987"});
+  }
+
 }
